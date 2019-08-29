c347ae0a7100dcf77b096e429dab82754cf5ead7
JonathanSCasper <jcasper@harding.edui>
2019-08-29 12:08:20 -0500

    Make Changes
    Made program work with given class

--- LightsOut/LightsOut/MainForm.cs
+++ LightsOut/LightsOut/MainForm.cs
@@ -8,59 +8,56 @@ namespace LightsOut
     {
         private const int GridOffset = 25;
         private const int GridLength = 200;
-        private const int NumCells = 3;
-        private const int CellLength = GridLength / NumCells;
+        //private const int NumCells = 3;
+        private LightsOutGame game;
+        private const int CellLength = GridLength / 3;
 
-        private bool[,] grid;
-        private Random rand;
+        //private bool[,] grid;
+        //private Random rand;
 
         public MainForm()
         {
             InitializeComponent();
 
-            rand = new Random();
-            grid = new bool[NumCells, NumCells];
+            game = new LightsOutGame();
+            game.NewGame();
 
-            for (int r = 0; r < NumCells; r++)
-                for (int c = 0; c < NumCells; c++)
-                    grid[r, c] = true;
+            //rand = new Random();
+            //grid = new bool[NumCells, NumCells];
+
+            //for (int r = 0; r < NumCells; r++)
+            //    for (int c = 0; c < NumCells; c++)
+            //        grid[r, c] = true;
         }
 
-        private void MainForm_Load(object sender, EventArgs e)
-        {
-
-        }
-
-        private void GameToolStripMenuItem_Click(object sender, EventArgs e)
-        {
-
-        }
         private void NewToolStripMenuItem_Click(object sender, EventArgs e)
         {
             newGameButton_Click(sender, e);
         }
-
-        private void Button1_Click(object sender, EventArgs e)
+        private void newGameButton_Click(object sender, EventArgs e)
         {
-            for (int r = 0; r < NumCells; r++)
-                for (int c = 0; c < NumCells; c++)
-                    grid[r, c] = rand.Next(2) == 1;
+            game.NewGame();
 
+            //for (int r = 0; r < NumCells; r++)
+            //    for (int c = 0; c < NumCells; c++)
+            //        grid[r, c] = rand.Next(2) == 1;
+            
             this.Invalidate();
         }
+
 
         private void MainForm_Paint(object sender, PaintEventArgs e)
         {
             Graphics g = e.Graphics;
 
-            for (int r = 0; r < NumCells; r++)
+            for (int r = 0; r < game.GridSize; r++)
             {
-                for (int c = 0; c < NumCells; c++)
+                for (int c = 0; c < game.GridSize; c++)
                 {
                     Brush brush;
                     Pen pen;
 
-                    if (grid[r, c])
+                    if (game.GetGridValue(r,c))
                     {
                         pen = Pens.Black;
                         brush = Brushes.White;
@@ -79,52 +76,55 @@ namespace LightsOut
                 }
             }
         }
+        //private bool PlayerWon()
+        //{
+        //    //bool won = true;
+        //    //for (int r = 0; r < NumCells; r++)
+        //    //{
+        //    //    for (int c = 0; c < NumCells; c++)
+        //    //    {
+        //    //        if (grid[r, c] == true)
+        //    //            won = false;
+        //    //    }
+        //    //}
+        //    //return won;
+        //}
+
+        //private void Button1_Click(object sender, EventArgs e)
+        //{
+        //    for (int r = 0; r < NumCells; r++)
+        //        for (int c = 0; c < NumCells; c++)
+        //            grid[r, c] = rand.Next(2) == 1;
+
+        //    this.Invalidate();
+        //}
 
         private void MainForm_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
         {
-            if (e.X < GridOffset || e.X > CellLength * NumCells + GridOffset ||
-            e.Y < GridOffset || e.Y > CellLength * NumCells + GridOffset)
+            if (e.X < GridOffset || e.X > CellLength * game.GridSize + GridOffset ||
+            e.Y < GridOffset || e.Y > CellLength * game.GridSize + GridOffset)
                 return;
             
             int r = (e.Y - GridOffset) / CellLength;
             int c = (e.X - GridOffset) / CellLength;
 
-            for (int i = r - 1; i <= r + 1; i++)
-                for (int j = c - 1; j <= c + 1; j++)
-                    if (i >= 0 && i < NumCells && j >= 0 && j < NumCells)
-                        grid[i, j] = !grid[i, j];
+            //for (int i = r - 1; i <= r + 1; i++)
+            //    for (int j = c - 1; j <= c + 1; j++)
+            //        if (i >= 0 && i < game.GridSize && j >= 0 && j < game.GridSize)
+            //            grid[i, j] = !grid[i, j];
+
+            game.Move(r, c);
 
             this.Invalidate();
 
-            if (PlayerWon())
+            if (game.IsGameOver())
             {
                 MessageBox.Show(this, "Congratulations! You've won!", "Lights Out!",
                     MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
-        private bool PlayerWon()
-        {
-            bool won = true;
-            for (int r = 0; r < NumCells; r++)
-            {
-                for (int c = 0; c < NumCells; c++)
-                {
-                    if (grid[r, c] == true)
-                        won = false;
-                }
-            }
-            return won;
-        }
 
-        private void newGameButton_Click(object sender, EventArgs e)
-        {
-            for (int r = 0; r < NumCells; r++)
-                for (int c = 0; c < NumCells; c++)
-                    grid[r, c] = rand.Next(2) == 1;
-            
-            this.Invalidate();
-        }
 
         private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
         {
@@ -140,6 +140,15 @@ namespace LightsOut
         {
             AboutForm aboutBox = new AboutForm();
             aboutBox.ShowDialog(this);
+        }
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
